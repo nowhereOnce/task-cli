1fa869dc070ac0c1e1ba74249e69dee98ba684c8
nowhereOnce <alejandro.aguilar.fi02@gmail.com>
2026-01-15 13:54:43 -0600

    feature: updatedAt attribute added.

--- task_cli/logic.py
+++ task_cli/logic.py
@@ -34,22 +34,30 @@ def list_tasks(status: str):
         tasks = [t for t in tasks if t["status"] == status]
 
     console = Console()
+
     table = Table(show_header=True, header_style="bold magenta")
     table.add_column("ID", style="dim", width=4)
     table.add_column("Description", min_width=20, max_width=35)
     table.add_column("Status", justify="right")
     table.add_column("Created at", width=10)
+    table.add_column("Updated at", width=10)
 
     for t in tasks:
         # Convert from string to datetime obj
-        t_date_obj = datetime.datetime.strptime(t["createdAt"], '%d-%m-%y %H:%M:%S.%f')
-        t_date = t_date_obj.date()
+        t_createdAt_date_obj = datetime.datetime.strptime(t["createdAt"], '%d-%m-%y %H:%M:%S.%f')
+        t_createdAt_date = t_createdAt_date_obj.date()
+
+        if t["updatedAt"] is not None:
+            t_updatedAt_date_obj = datetime.datetime.strptime(t["updatedAt"], '%d-%m-%y %H:%M:%S.%f')
+            t_updatedAt_date = t_updatedAt_date_obj.date()
 
         color = "green" if t["status"] == "done" else "yellow"
         table.add_row(str(t["id"]), 
                       t["description"], 
                       f"[{color}]{t['status']}[/]",
-                      t_date.strftime('%d-%m-%y'))
+                      t_createdAt_date.strftime('%d-%m-%y'),
+                      t_updatedAt_date.strftime('%d-%m-%y') if t["updatedAt"] is not None else "",
+                      )
     
     console.print(table)
 
@@ -93,6 +101,7 @@ def update_task(task_id: int, description = None):
     if description != task["description"]: # if changes were made    
         # Update data 
         task["description"] = description
+        task["updatedAt"] = datetime.datetime.now().strftime('%d-%m-%y %H:%M:%S.%f') # update time
         save_tasks(data)
         print(f"Task {task_id} updated successfully.")
     else:
